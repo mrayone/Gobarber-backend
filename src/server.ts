2c4431362c3f1a17b4440dfaf745fe13e55dea22
Maycon Rayone <maycon.rayone@gmail.com>
2020-05-21 20:56:15 -0300

    adicionando middlware de cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import AppError from './errors/AppError';
 import routes from './routes';
 import uploadConfig from './config/upload';
 import './database';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.homeDirectory));
 app.use(routes);
