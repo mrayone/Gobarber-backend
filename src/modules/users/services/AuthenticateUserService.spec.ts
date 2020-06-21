67ce3b51f741fa59f5cce3fe9c0c9e8be4118060
Maycon Rayone <maycon.rayone@gmail.com>
2020-06-21 00:31:29 -0300

    validação para autenticação do usuário.

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,4 +1,4 @@
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 import CreateUserService from './CreateUserService';
 import AuthenticateUserService from './AuthenticateUserService';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
@@ -29,5 +29,49 @@ describe('AuthenticateUserService', () => {
     });
 
     expect(response).toHaveProperty('token');
+  });
+
+  it('Should not be able autenthicate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUserService = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      authenticateUserService.execute({
+        email: 'john.doe@gmail.com',
+        password: '124578',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should not be able autenthicate when email and password not match', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const createUsersService = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const authenticateUserService = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUsersService.execute({
+      email: 'john.doe@gmail.com',
+      name: 'John Doe',
+      password: '124578',
+    });
+
+    expect(
+      authenticateUserService.execute({
+        email: 'john.doe@gmail.com',
+        password: '1245788',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
